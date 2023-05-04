6156345ab028ad4983eda22c92f745a6ae3aa9a7
Tanishq <tanishqbakka1@gmail.com>
2023-05-04 08:13:41 +0530

    main

--- main.py
+++ main.py
@@ -9,20 +9,8 @@ gameSpeed=15
 arrowList=pygame.sprite.Group()
 recieveList=pygame.sprite.Group()
 highlightList=pygame.sprite.Group()
-
-class highlightSprite(pygame.sprite.Sprite):
-    def __init__(self,rotation):
-        super().__init__()
-        self.image=pygame.image.load("src/game_files/game_objects/arrow_light.png")
-        self.image=pygame.transform.rotate(self.image,rotation+90)
-        self.image=pygame.transform.smoothscale(self.image,(85,85))
-        self.rect = self.image.get_rect()
-        self.rect.center=rotation+50,80
-        screen.blit(self.image,self.rect)
-    def die(self):
-           self.image=pygame.transform.smoothscale_by(self.image,90,2)
-           self.kill()
-
+gameScore=0
+arrowBlink=[pygame.image.load("src/game_files/game_objects/arrow_light_4.png"),pygame.image.load("src/game_files/game_objects/arrow_light_3.png"),pygame.image.load("src/game_files/game_objects/arrow_light_2.png"),pygame.image.load("src/game_files/game_objects/arrow_light_1.png"),pygame.image.load("src/game_files/game_objects/arrow_light_0.png")]
 class spawnCollector(pygame.sprite.Sprite):
     def __init__(self,rotation):
          super().__init__()
@@ -53,8 +41,6 @@ class spawnCollector(pygame.sprite.Sprite):
          if self.rect.x==280 and not key[pygame.K_UP]:
              self.rect.y=40
           
-
-                
 class spawnArrow(pygame.sprite.Sprite):
     def __init__(self,color,rotation):
         super().__init__()
@@ -67,20 +53,23 @@ class spawnArrow(pygame.sprite.Sprite):
         screen.blit(self.image,self.rect)      
         
     def update(self):
+        global gameScore
         self.rect.y -= 1
-        if self.rect.y<47:
+        rotation=self.rect.angle
+        if self.rect.y<50:
              key=pygame.key.get_pressed()
              if key[pygame.K_LEFT] and self.rect.x==15:
-                print("score") 
+                gameScore+=1
+                
                 self.kill()
              if key[pygame.K_DOWN] and self.rect.x==105:
-                print("score") 
+                gameScore+=1
                 self.kill()
              if key[pygame.K_RIGHT] and self.rect.x==195:
-                print("score") 
+                gameScore+=1
                 self.kill()
              if key[pygame.K_UP] and self.rect.x==285:
-                print("score") 
+                gameScore+=1
                 self.kill()
              self.rect.y -= 1
              if self.rect.y<40:
@@ -90,8 +79,9 @@ for i in range(0,4):
     reciever=spawnCollector(i*90)
     recieveList.add(reciever)
 
-print(recieveList)
+
 while not Done:
+    print(gameScore)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
@@ -103,9 +93,7 @@ while not Done:
            color=str(random.randrange(0,8))
         arrow=spawnArrow(color,rotation)
         arrowList.add(arrow)
-        
-    
-    
+       
     screen.fill((0, 0, 0))
     pygame.time.wait(gameSpeed)
     recieveList.update()
